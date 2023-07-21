1d9280ff67adf3887e6108153dd0a70a68bc611b
rovinmendoza-2 <137201185+rovinmendoza-2@users.noreply.github.com>
2023-07-21 13:18:57 -0600

    validación de email backend

--- backend/controllers/usuarioController.js
+++ backend/controllers/usuarioController.js
@@ -1,26 +1,34 @@
-const Usuario = require('../models/usuario');
-const bcrypt = require('bcrypt-nodejs');
+const Usuario = require("../models/usuario");
+const bcrypt = require("bcrypt-nodejs");
 
-const register_user_admin = async(req, res) => {
-    console.log(req.body);
+const register_user_admin = async (req, res) => {
+  console.log(req.body);
 
-    const data = req.body;
+  const data = req.body;
 
-    bcrypt.hash('123456', null, null, async(err, hash) => {
-        if(err){
-            res.status(200).send(
-                {
-                    data:undefined,
-                    message: 'No se pudo encriptar la contrasena'
-                });
-        }else{
-            data.password = hash;
-            const usuario = await Usuario.create(data);
-            res.status(200).send({data:usuario});
-        }
+  const usuarios = await Usuario.find({ email: data.email });
+
+  console.log(usuarios);
+  if (usuarios.length >= 1) {
+    res
+      .status(200)
+      .send({ data: undefined, message: "El correo electronico ya existe" });
+  } else {
+    bcrypt.hash("123456", null, null, async (err, hash) => {
+      if (err) {
+        res.status(200).send({
+          data: undefined,
+          message: "No se pudo encriptar la contrasena",
+        });
+      } else {
+        data.password = hash;
+        const usuario = await Usuario.create(data);
+        res.status(200).send({ data: usuario });
+      }
     });
+  }
 };
 
 module.exports = {
-    register_user_admin
-}
+  register_user_admin,
+};
--- backend/models/usuario.js
+++ backend/models/usuario.js
@@ -6,7 +6,9 @@ const UsuarioSchema = Schema({
     lastName:  {type: String, required: true},
     email:      {type: String, required: true, unique: true},
     role:        {type: String, required: true},
-    password:   {type: String, required: true}
+    password:   {type: String, required: true},
+    state: {type: Boolean, default: true, require: true},
+    createdAt: {type: Date, default: Date.now}
 });
 
 module.exports = mongoose.model('usuario', UsuarioSchema);
