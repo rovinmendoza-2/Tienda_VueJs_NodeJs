2cef71048542a0c5c2a82d45e782a90bbc9df7a2
rovinmendoza-2 <137201185+rovinmendoza-2@users.noreply.github.com>
2023-07-21 11:05:23 -0600

    conexión base de datos

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
 const port = process.env.port || 4200;
 
-app.listen(port, () => {
-    console.log("servidor corriendo en", port);
-});
+//Conexion a base de datos
+mongoose.connect('mongodb://127.0.0.1:27017/tienda')
+  .then(() => {
+    console.log('Conexión exitosa a la base de datos');
+    app.listen(port, () => {
+        console.log("El servidor se ejecuto en el puerto 4200");
+    });
+  })
+  .catch(error => {
+    console.error('Error al conectar a la base de datos:', error);
+  });
+
+
 
 
 module.exports = app;
