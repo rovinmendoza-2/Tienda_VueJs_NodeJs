5db5d308fd99650e72346ac889531b0a12fd6cbe
rovinmendoza-2 <137201185+rovinmendoza-2@users.noreply.github.com>
2023-07-23 08:18:15 -0600

    componente TopNavPage

--- panel/src/views/RegisterCollaborator/RegisterPage.vue
+++ panel/src/views/RegisterCollaborator/RegisterPage.vue
@@ -2,41 +2,7 @@
     <div>
         <SidebarPage></SidebarPage>
         <div class="main-content">
-
-            <nav class="navbar navbar-expand-md navbar-light d-md-flex">
-                <div class="container-fluid">
-
-                <!-- Form -->
-                <form class="form-inline me-4 d-none d-md-flex">
-                    
-                </form>
-
-                <!-- User -->
-                <div class="navbar-user">
-
-
-                    <!-- Dropdown -->
-                    <div class="dropdown">
-
-                    <!-- Toggle -->
-                    <a href="#" class="avatar avatar-sm avatar-online dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <img src="/assets/img/avatar-1.jpg" alt="..." class="avatar-img rounded-circle" style="-webkit-mask-image: none;">
-                    </a>
-
-                    <!-- Menu -->
-                    <div class="dropdown-menu dropdown-menu-end">
-                        <a href="./profile-posts.html" class="dropdown-item">Profile</a>
-                        <a href="./account-general.html" class="dropdown-item">Settings</a>
-                        <hr class="dropdown-divider">
-                        <a href="./sign-in.html" class="dropdown-item">Logout</a>
-                    </div>
-
-                    </div>
-
-                </div>
-
-                </div>
-            </nav>
+            <TopNavPage></TopNavPage>
 
             <div class="container-fluid">
                 <div class="row justify-content-center">
@@ -194,11 +160,13 @@
 <script>
 // @ is an alias to /src
 import SidebarPage from '../../components/SidebarPage.vue';
+import TopNavPage from '../../components/TopNavPage.vue';
 
 export default {
   name: 'RegisterCollaborator',
   components: {
-    SidebarPage
+    SidebarPage,
+    TopNavPage
   }
 }
 </script>
