f9f89ecb721a9fc3700927c53344e368f13a1f68
rovinmendoza-2 <137201185+rovinmendoza-2@users.noreply.github.com>
2023-08-04 17:17:32 -0600

    creando esquema de variedad

---
+++ backend/models/variedad.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const VarietydSchema = Schema({
+  supplier:  { type: String, required: true },
+  variety:   { type: String, required: true },
+  sku:       { type: String, required: true },
+  product:   { type: Schema.ObjectId, ref: "product", required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("varietyd", VarietydSchema);
