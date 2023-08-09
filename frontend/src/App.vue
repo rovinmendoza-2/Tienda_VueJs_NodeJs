855cf6064da631acee1b1b1b78c721789cd45a97
rovinmendoza-2 <137201185+rovinmendoza-2@users.noreply.github.com>
2023-08-09 09:40:14 -0600

    configuración inicial de la tienda ecomerce

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,32 +1,23 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
+    <HeaderPage></HeaderPage>
+    
     <router-view/>
+
+    <FooterPage></FooterPage>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import HeaderPage from './components/HeaderPage.vue';
+import FooterPage from './components/FooterPage.vue';
 
-nav {
-  padding: 30px;
-}
+export default {
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+  components: {
+    HeaderPage,
+    FooterPage
 }
+}
+</script>
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
