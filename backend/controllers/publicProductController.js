fc558791d864acba48385c81d0b077d1ea72e05c
rovinmendoza-2 <137201185+rovinmendoza-2@users.noreply.github.com>
2023-08-10 12:11:43 -0600

    obtener productos públicos para el ecomerce

--- backend/controllers/publicProductController.js
+++ backend/controllers/publicProductController.js
@@ -1,10 +1,16 @@
 const Product = require('../models/products');
 
 const get_new_product = async(req, res)=> {
-    const product = await Product.find().sort({createdAt: -1}).limit(4);
+    const product = await Product.find({state:true}).sort({createdAt: -1}).limit(4);
+    res.status(200).send(product);
+};
+
+const get_product_recommended = async(req, res)=> {
+    const product = await Product.find({state:true}).limit(8);
     res.status(200).send(product);
 };
 
 module.exports = {
     get_new_product,
+    get_product_recommended
 }
--- backend/routes/public.js
+++ backend/routes/public.js
@@ -4,5 +4,6 @@ const publicProductController = require('../controllers/publicProductController'
 const api = express.Router();
 
 api.get('/get_new_product', publicProductController.get_new_product);
+api.get('/get_product_recommended', publicProductController.get_product_recommended);
 
 module.exports = api;
