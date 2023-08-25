dfb571963c1b4263da41661f8ff81c5df98334f1
rovinmendoza-2 <137201185+rovinmendoza-2@users.noreply.github.com>
2023-08-25 11:24:33 -0600

    creación de modelo de dirección

---
+++ backend/models/address.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const AddresSchema = Schema({
+  name:         { type: String, required: true },
+  lastName:     { type: String, required: true },
+  idDocument:   { type: String, required: true },
+  phone:        { type: String, required: true },
+  country:        { type: String, required: true },
+  city:       { type: String, required: true },
+  address:      { type: String, required: true },
+  customer:     { type: Schema.ObjectId, ref: 'customer', required: true },
+  createdAt:    { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("addres", AddresSchema);
