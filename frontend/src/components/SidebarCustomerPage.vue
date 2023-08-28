4b63b6a6509e18790bc37260f977e8675c1c458d
rovinmendoza-2 <137201185+rovinmendoza-2@users.noreply.github.com>
2023-08-28 17:07:28 -0600

    menú dinámico para cliente

---
+++ frontend/src/components/SidebarCustomerPage.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <div class="customer-sidebar card border-0">
+            <div class="customer-profile">
+                <a class="d-inline-block" href="#"><img class="img-fluid rounded-circle customer-image shadow"
+                        src="https://d19m59y37dris4.cloudfront.net/sell/2-0/img/photo/kyle-loftus-589739-unsplash-avatar.jpg"
+                        alt="" /></a>
+                <h5>Julie Trnka</h5>
+                <p class="text-muted text-sm mb-0">Ostrava, Czech Republic</p>
+            </div>
+            <nav class="list-group customer-nav">
+                <a class="list-group-item d-flex justify-content-between align-items-center text-decoration-none"
+                    href="customer-orders.html"><span>
+                        <svg class="svg-icon svg-icon-heavy me-2">
+                            <use xlink:href="#paper-bag-1"></use>
+                        </svg>Orders</span>
+                    <div class="badge rounded-pill bg-dark fw-normal px-3">
+                        5
+                    </div>
+                </a>
+                <a class="list-group-item d-flex justify-content-between align-items-center text-decoration-none"
+                    href="customer-account.html"><span>
+                    <svg class="svg-icon svg-icon-heavy me-2">
+                        <use xlink:href="#male-user-1"></use>
+                    </svg>Profile</span>
+                </a>
+                <router-link to="/cuenta/direcciones" active-class="active" class="list-group-item d-flex justify-content-between align-items-center text-decoration-none">
+                    <span>
+                    <svg class="svg-icon svg-icon-heavy me-2">
+                        <use xlink:href="#navigation-map-1"></use>
+                    </svg>Direcciones</span>
+                </router-link>
+                <a class="list-group-item d-flex justify-content-between align-items-center text-decoration-none"
+                    href="customer-login.html"><span>
+                    <svg class="svg-icon svg-icon-heavy me-2">
+                        <use xlink:href="#exit-1"></use>
+                    </svg>Log out</span>
+                </a>
+            </nav>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SidebarCustomerPage',
+    data() {
+        return {
+
+        }
+    },
+
+}
+</script>
--- frontend/src/views/Address/AddressPage.vue
+++ frontend/src/views/Address/AddressPage.vue
@@ -122,42 +122,7 @@
                     </div>
                     <!-- Customer Sidebar-->
                     <div class="col-xl-3 col-lg-4 mb-5">
-                        <div class="customer-sidebar card border-0">
-                            <div class="customer-profile">
-                                <a class="d-inline-block" href="#"><img
-                                        class="img-fluid rounded-circle customer-image shadow"
-                                        src="https://d19m59y37dris4.cloudfront.net/sell/2-0/img/photo/kyle-loftus-589739-unsplash-avatar.jpg"
-                                        alt="" /></a>
-                                <h5>Julie Trnka</h5>
-                                <p class="text-muted text-sm mb-0">Ostrava, Czech Republic</p>
-                            </div>
-                            <nav class="list-group customer-nav">
-                                <a class="list-group-item d-flex justify-content-between align-items-center text-decoration-none"
-                                    href="customer-orders.html"><span>
-                                        <svg class="svg-icon svg-icon-heavy me-2">
-                                            <use xlink:href="#paper-bag-1"></use>
-                                        </svg>Orders</span>
-                                    <div class="badge rounded-pill bg-dark fw-normal px-3">
-                                        5
-                                    </div>
-                                </a><a
-                                    class="list-group-item d-flex justify-content-between align-items-center text-decoration-none"
-                                    href="customer-account.html"><span>
-                                        <svg class="svg-icon svg-icon-heavy me-2">
-                                            <use xlink:href="#male-user-1"></use>
-                                        </svg>Profile</span></a><a
-                                    class="active list-group-item d-flex justify-content-between align-items-center text-decoration-none"
-                                    href="customer-addresses.html"><span>
-                                        <svg class="svg-icon svg-icon-heavy me-2">
-                                            <use xlink:href="#navigation-map-1"></use>
-                                        </svg>Addresses</span></a><a
-                                    class="list-group-item d-flex justify-content-between align-items-center text-decoration-none"
-                                    href="customer-login.html"><span>
-                                        <svg class="svg-icon svg-icon-heavy me-2">
-                                            <use xlink:href="#exit-1"></use>
-                                        </svg>Log out</span></a>
-                            </nav>
-                        </div>
+                        <SidebarCustomerPage></SidebarCustomerPage>
                     </div>
                     <!-- /Customer Sidebar-->
                 </div>
@@ -168,6 +133,7 @@
 
 <script>
 import axios from 'axios';
+import SidebarCustomerPage from '@/components/SidebarCustomerPage.vue';
 
 export default {
     name: 'AddressPage',
@@ -242,6 +208,10 @@ export default {
     },
     beforeMount(){
         this.init_data();
+    },
+
+    components: {
+        SidebarCustomerPage,
     }
 }
 </script>
